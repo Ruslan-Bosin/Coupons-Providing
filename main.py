c5b44f36e62ad619b9f0e5934cc15457c07adade
G1ebS <glev.shooter06@gmail.com>
2022-10-26 20:14:31 +0300

    Удалил мусор(здесь должно быть оскорбление, но мне лень)

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from flask import Flask
 
-
 app: Flask = Flask(__name__)
 
 
 @app.route("/")
 def index() -> str:
     return "Index Page"
-
-
-@app.route("/hello")
-def hello() -> str:
-    return "HELLo"
-
-
-@app.route("/hi")
-def hi() -> str:
-    return "Hi"
 
 
 if __name__ == "__main__":
